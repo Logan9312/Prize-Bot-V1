051517b859c464ebcfd910723da98285314afd73
Logan <lgntravis@gmail.com>
2021-06-14 21:24:09 -0600

    adding more slash command functionality

--- slashcommands/help.go
+++ slashcommands/help.go
@@ -4,32 +4,48 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-func Help (s *discordgo.Session, i *discordgo.InteractionCreate) {
+func Help(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Components:      []discordgo.MessageComponent{},
-			Embeds:          []*discordgo.MessageEmbed{
+			Components: []discordgo.MessageComponent{
+				discordgo.ActionsRow{
+					Components: []discordgo.MessageComponent{
+						discordgo.Button{
+							Label:    "Close Help Menu",
+							Style:    3,
+							Disabled: false,
+							Emoji: discordgo.ButtonEmoji{
+								Name:     "",
+								ID:       "",
+								Animated: false,
+							},
+							CustomID: "closehelp",
+						},
+					},
+				},
+			},
+			Embeds: []*discordgo.MessageEmbed{
 				{
 					URL:         "",
 					Type:        "",
 					Title:       "Discord Bot Help",
 					Description: "This bot currently only supports Auctions.",
-					Timestamp: "",
-					Color:     0,
-					Footer:    &discordgo.MessageEmbedFooter{},
-					Image:     &discordgo.MessageEmbedImage{},
-					Thumbnail: &discordgo.MessageEmbedThumbnail{},
-					Video:     &discordgo.MessageEmbedVideo{},
-					Provider:  &discordgo.MessageEmbedProvider{},
-					Author:    &discordgo.MessageEmbedAuthor{},
-					Fields:    []*discordgo.MessageEmbedField{
+					Timestamp:   "",
+					Color:       0,
+					Footer:      &discordgo.MessageEmbedFooter{},
+					Image:       &discordgo.MessageEmbedImage{},
+					Thumbnail:   &discordgo.MessageEmbedThumbnail{},
+					Video:       &discordgo.MessageEmbedVideo{},
+					Provider:    &discordgo.MessageEmbedProvider{},
+					Author:      &discordgo.MessageEmbedAuthor{},
+					Fields: []*discordgo.MessageEmbedField{
 						{
-							Name: "`/Help`:",
+							Name:  "`/Help`:",
 							Value: "A command that displays bot functionality.",
 						},
 						{
-							Name: "`/Auction`:",
+							Name:  "`/Auction`:",
 							Value: "Allows you to put an item up for auction.",
 						},
 					},
@@ -39,4 +55,11 @@ func Help (s *discordgo.Session, i *discordgo.InteractionCreate) {
 			Flags:           0,
 		},
 	})
+
+	switch i.MessageComponentData().CustomID {
+	case "1":
+		content += "Bid Successful"
+	case "2":
+		content += " no"
+	}
 }
