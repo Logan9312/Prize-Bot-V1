296251d1d56232a6a650762df34c5f44543d3be6
Logan <lgntravis@gmail.com>
2021-08-10 00:43:05 -0600

    hmmmmmm

---
+++ connect/base.go
@@ -0,0 +1,70 @@
+package connect
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+	"gitlab.com/logan9312/discord-auction-bot/commands"
+)
+
+var prodCommands = []*discordgo.ApplicationCommand{
+	&commands.HelpCommand,
+}
+
+var localCommands = []*discordgo.ApplicationCommand{
+	&commands.HelpCommand,
+	&commands.ProfileCommand,
+	&commands.AuctionCommand,
+}
+
+func BotConnect(token, environment, botName string) {
+
+	fmt.Println(botName + " Starting Up...")
+
+	s, err := discordgo.New("Bot " + token)
+
+	prodCommands = append(prodCommands, commands.ReviewCommands[0], commands.ReviewCommands[1])
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Builds local commands
+	if environment == "local" {
+
+		for _, v := range localCommands {
+			v.Description = "EXPERIMENTAL: " + v.Description
+		}
+
+		for _, v := range s.State.Guilds {
+			_, err = s.ApplicationCommandBulkOverwrite(s.State.User.ID, v.ID, localCommands)
+			fmt.Println("Commands added to guild: " + v.Name)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		commands.HelpBuilder(localCommands)
+	}
+
+	//Builds prod commands
+	if environment == "prod" {
+
+		_, err = s.ApplicationCommandBulkOverwrite(s.State.User.ID, "", prodCommands)
+		if err != nil {
+			fmt.Println(err)
+		}
+		commands.HelpBuilder(prodCommands)
+	}
+
+	err = s.Open()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	BotStatus(s)
+
+	defer fmt.Println(botName + " Startup Complete!")
+}
--- main.go
+++ main.go
@@ -5,24 +5,10 @@ import (
 	"log"
 
 	"github.com/caarlos0/env"
-	"gitlab.com/logan9312/discord-auction-bot/commands"
+	"gitlab.com/logan9312/discord-auction-bot/connect"
 	//"gitlab.com/logan9312/discord-auction-bot/database"
 	"gitlab.com/logan9312/discord-auction-bot/routers"
-	"github.com/bwmarrin/discordgo"
 )
-
-var prodCommands = []*discordgo.ApplicationCommand{
-	&commands.HelpCommand,
-
-}
-
-var localCommands = []*discordgo.ApplicationCommand{
-	&commands.HelpCommand,
-	&commands.ProfileCommand,
-	&commands.AuctionCommand,
-}
-
-
 
 // Environment struct
 type Environment struct {
@@ -42,42 +28,9 @@ func main() {
 		log.Fatal("FAILED TO LOAD ENVIRONMENT VARIABLES")
 	}
 
-	s, err := discordgo.New("Bot " + environment.DiscordToken)
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	//Connects main bot
-	//BotConnect(environment.DiscordToken, environment.Environment, "Main Bot")
-
-		//Builds local commands
-		if environment.Environment == "local" {
-
-			for _, v := range localCommands {
-				v.Description = "EXPERIMENTAL: " + v.Description
-			}
+	connect.BotConnect(environment.DiscordToken, environment.Environment, "Main Bot")
 	
-			for _, v := range s.State.Guilds {
-				_, err = s.ApplicationCommandBulkOverwrite(s.State.User.ID, v.ID, localCommands)
-				fmt.Println("Commands added to guild: " + v.Name)
-				if err != nil {
-					fmt.Println(err)
-				}
-			}
-			commands.HelpBuilder(localCommands)
-		}
-	
-		//Builds prod commands
-		if environment.Environment == "prod" {
-	
-			_, err = s.ApplicationCommandBulkOverwrite(s.State.User.ID, "", prodCommands)
-			if err != nil {
-				fmt.Println(err)
-			}
-			commands.HelpBuilder(prodCommands)
-		}
-	
-
 	//Connects Sir Grungerson
 	// BotConnect(environment.Grungerson, environment.Environment, "Sir Grungerson")
 
