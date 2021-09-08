7714ab1248bb8726cfa134211b027fb0103e0559
Logan <lgntravis@gmail.com>
2021-09-08 23:05:01 +0000

    Update auction.go

--- commands/auction.go
+++ commands/auction.go
@@ -363,7 +363,7 @@ func AuctionBid(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	}	
 	s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
-		Data: discordgo.InteractionResponseData{
+		Data: &discordgo.InteractionResponseData{
 			Content: "hooray it works",
 		},
 	})
