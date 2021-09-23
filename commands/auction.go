2d6ee77a9f04b5622422e9928738173f005f4c88
Logan <lgntravis@gmail.com>
2021-09-23 14:42:49 +0000

    Update auction.go

--- commands/auction.go
+++ commands/auction.go
@@ -431,8 +431,8 @@ func AuctionCreate(s *discordgo.Session, i *discordgo.InteractionCreate) {
 					discordgo.Button{
 Label: "Clear Auction Chat",
 Style: 4,
-CustomID: "clearchat"
-Disabled: true
+CustomID: "clearchat",
+Disabled: true,
 					},
 				},
 			},
