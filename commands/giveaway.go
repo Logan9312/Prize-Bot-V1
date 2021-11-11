cca21b37fec7a04c1e43203b16b13e93f1f3575a
Logan <lgntravis@gmail.com>
2021-11-10 17:42:01 -0700

    disabling reroll until I can fix

--- commands/giveaway.go
+++ commands/giveaway.go
@@ -454,7 +454,7 @@ func GiveawayEnd(s *discordgo.Session, messageID string) {
 					discordgo.Button{
 						Label:    "Reroll",
 						Style:    1,
-						Disabled: false,
+						Disabled: true,
 						CustomID: "reroll_giveaway",
 					},
 				},
