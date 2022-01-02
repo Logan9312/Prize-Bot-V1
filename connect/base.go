92b0289bedd3fc0907c0da028ae1d986bb802d19
Logan <lgntravis@gmail.com>
2022-01-01 23:49:46 -0700

    v0.9.4

--- connect/base.go
+++ connect/base.go
@@ -82,7 +82,7 @@ func BotConnect(token, environment, botName string) {
 
 	Timers(s)
 
-	err = s.UpdateGameStatus(0, "Bot Version v0.9.3")
+	err = s.UpdateGameStatus(0, "Bot Version v0.9.4")
 	if err != nil {
 		fmt.Println(err)
 		return
