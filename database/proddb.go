1f917b7e62ae7ae973606d5b61758dc935495835
Logan Travis <lgntravis@gmail.com>
2025-07-06 11:29:22 -0600

    Changing to neon db

--- database/proddb.go
+++ database/proddb.go
@@ -2,18 +2,16 @@ package database
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ProdDB(password, host string) *gorm.DB {
-	dbuser := "auctionbot"
-	port := "3306"
-	dbname := "auction"
+	dbURL := os.Getenv("DATABASE_URL")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, port, dbuser, dbname, password)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
 	}
