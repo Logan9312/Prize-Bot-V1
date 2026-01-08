1b7b68f217917a9f0555d7acbd66db9007c0e181
Logan Travis <lgntravis@gmail.com>
2026-01-08 13:01:13 -0700

    Adapter svelte

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter({})
 	}
 };
 
